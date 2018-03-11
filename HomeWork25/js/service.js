838712e2bc4ec3ec5f3e28b91d8e52883957ee58
andruxgit <jobandrux@yandex.ru>
2018-03-12 02:31:48 +0300

    0312

--- HomeWork25/js/service.js
+++ HomeWork25/js/service.js
@@ -1,55 +1,48 @@
 'use strict';
 (function init (){
 	const DOMauthPage = ` 
-<div id = 'main-context' class="text-center row" role="main">
-    <div class = "container col-8">
-        <div id = "wrap-modal" class="alert alert-danger col-5" role="alert">
-            This is a danger alert—check it out!
-        </div>
-    </div>
-    <form id = "form-input" class="form">
-
-        <h1 class="h3 mb-3 font-weight-normal">Заполните поля</h1>
-        <label for="input-email" class="input-title  control-label">Email</label>
-        <input type="text" id="input-email" class="form-control col" placeholder="Email address" required="" 
-        autofocus=""  autocomplete="off">
-        <label for="input-pass" class="input-title  control-label">Пароль</label>
-        <input type="text" id="input-pass" class="form-control col" placeholder="Password" required="" 
-        autocomplete="off">
-        <div class="checkbox mb-3">
-            <!--<label>-->
-                <!--<input type="checkbox" value="remember-me"> Remember me-->
-            <!--</label>-->
-        </div>
-        <div class = row>
-            <button id ="auth-button" class="btn btn-lg btn-primary btn-block col" type="button">Авторизация</button>
-        </div>
-        <!--<p class="mt-5 mb-3 text-muted">© 2017-2018</p>-->
-        </form>
- </div>`;
+<div id = 'main-context' class="col-md-4 col-12 col-md-offset-4" role="main">
+          <div class = "col-center">
+              <div id = "wrap-modal"  class="alert alert-danger" role="alert">
+                  This is a danger alert—check it out!
+              </div>
+          </div>
+          <form id = "form-input" class="form well">
+              <h1 class="h2  font-weight-normal ">Заполните поля</h1>
+              <label for="input-email" class="input-title  control-label">Email</label>
+              <input type="text" id="input-email" class="form-control col" placeholder="Email address" required=""
+                     autofocus=""  autocomplete="off">
+              <label for="input-pass" class="input-title  control-label">Пароль</label>
+              <input type="text" id="input-pass" class="form-control col" placeholder="Password" required=""
+                     autocomplete="off">
+                  <button id ="auth-button" class="btn btn-lg btn-primary btn-block" type="button">Авторизация</button>
+          </form>
+      </div>`;
 	const DOMuserPage =  `
- <div class = "userContent container">
-	<div class="row">
-            <h1 class="h3 font-weight-normal text-center ">Информация о пользователе</h1>
-    </div>   
-    <div class="row infoBlok">
-        <div class="col-md-5 text-left">
-            <label for = "otputEmail" class = "control-label">Логин</label>
-            <input type="text" id="otputEmail" class="form-control" placeholder="Email address">
-        </div>
-        <div class="text-left">
-            <label for = "userPassword" class = "control-label">Пароль</label>
-            <div class = "input-group row">
-                <input type="text" id="userPassword" class="form-control col" placeholder = "Password">
-                <span class = "input-group-btn">
-                    <button id="btnHide" class="btn btn-primary btn-block col" type="button">Показать 
+      <div class = " well userContent col-md-6  col-md-offset-3 col-12 col-offset-0">
+          <div class="row">
+              <h1 class="h3 font-weight-normal text-center ">Информация о пользователе</h1>
+          </div>
+          <div class="row infoBlok">
+              <div class="col-md-5 text-center">
+                  <label for = "otputEmail" class = "control-label">Логин</label>
+                  <input type="text" id="otputEmail" class="form-control" placeholder="Email address">
+              </div>
+              <label for = "userPassword" class = "control-label">Пароль</label>
+              <div class = "input-group container-fluid">
+                   <input type="text" id="userPassword" class="form-control" placeholder = "Password">
+                    <span class = "input-group-btn">
+                        <button id="btnHide" class="btn btn-primary btn-block" type="button">Показать
                     пароль</button>
-                </span>
-            </div>
-        </div>
-  </div>
-    <button id="btnComeBack" class="btn-lg btn-primary btn-block mt-2" type="button">Назад</button>
-  </div>`;
+                    </span>
+              </div>
+          </div>
+          <div class ="row">
+                <div class="row col-md-6 col-md-offset-3">
+                   <button id="btnComeBack" class="btn-lg btn-primary btn-block col-md-6" type="button">Назад</button>
+                </div>
+           </div>
+       </div>`;
 
 	const DOMmain = document.querySelector('#mainPage');
 	localStorage.setItem('emailValid','t@t.ru' );
